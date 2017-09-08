72dcf2c887dfe7c2a57d5bf6cb76440a936e05ef
Christie Villanueva <christiejvillanueva@gmail.com>
2017-09-08 10:59:04 -0700

    Implement working dynamic grocery list

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// TODO
+// var GroceryList = (item) => (
+//   <ul>
+//     <GroceryListItem />
+//   </ul>
+// );
+
+// // var GroceryList = () => (
+// //   <ul>
+// //     <GroceryListItem />
+// //     <Kale />
+// //   </ul>
+// // );
+
+// // var Cucumbers = () => <li>Cucumbers</li>;
+// // var Kale = () => <li>Kale</li>;
+
+// var GroceryListItem = (props.item) => (
+//   <li>{props.item}</li>
+// )
+
+var GroceryListItem = (props) => (
+   <li>{props.item}</li>
+)
+
+ var GroceryList = () => (
+   <ul>
+     <GroceryListItem item={'kale'} />
+     <GroceryListItem item={'turnip'} />
+   </ul>
+ );
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
